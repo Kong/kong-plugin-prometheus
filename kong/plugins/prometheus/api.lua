5dd1a51ed5636a7ab6c0a765495437c36fdcd058
Javier Guerra <javier.guerra@konghq.com>
2020-11-07 20:22:24 -0500

    use minimal RPC intead of mini-http

--- kong/plugins/prometheus/api.lua
+++ kong/plugins/prometheus/api.lua
@@ -1,39 +1,20 @@
 local prometheus = require "kong.plugins.prometheus.exporter"
+local stream_available, stream_api = pcall(require, "kong.tools.stream_api")
 
-if ngx.config.subsystem == "stream" then
-  local stream_api = require "kong.tools.stream_api"
+return {
+  ["/metrics"] = {
+    GET = function()
+      ngx.header.content_type = "text/plain; charset=UTF-8"
 
-  stream_api.register_endpoint("/stream-metrics", function(req)
-    req:get_headers()
-    return req:response(200, { ["Content-Type"] = "text/plain; charset=UTF-8" },
-            prometheus:collect())
-  end)
+      ngx.print(prometheus:collect())
 
-else
-  local resty_http = require "resty.http"
-  local STREAM_API_PORT = 8086    -- TODO: get from config
+      if stream_available then
+        ngx.print(stream_api.request("prometheus", ""))
+      end
+    end,
+  },
 
-  local function pull_from_stream()
-    local httpc = resty_http.new()
-    assert(httpc:connect("localhost", STREAM_API_PORT))
-
-    local res = assert(httpc:request({ method = "GET", path="/stream-metrics" }))
-    local body = res:read_body()
-
-    httpc:set_keepalive(false)
-
-    return body
-  end
-
-
-  return {
-    ["/metrics"] = {
-      GET = function()
-        ngx.header.content_type = "text/plain; charset=UTF-8"
-
-        ngx.say(prometheus:collect())
-        ngx.say(pull_from_stream())
-      end,
-    },
-  }
-end
+  _stream = stream_available and function()
+    return prometheus:collect()
+  end or nil,
+}
--- kong/plugins/prometheus/serve.lua
+++ kong/plugins/prometheus/serve.lua
@@ -1,6 +1,8 @@
 local lapis = require "lapis"
 local prometheus = require "kong.plugins.prometheus.exporter"
 
+local stream_available, stream_api = pcall(require, "kong.tools.stream_api")
+
 
 local kong = kong
 
@@ -37,7 +39,13 @@ end)
 
 
 app:match("/metrics", function()
-  prometheus:collect()
+  ngx.header.content_type = "text/plain; charset=UTF-8"
+
+  ngx.print(prometheus:collect())
+
+  if stream_available then
+    ngx.print(stream_api.request("prometheus", "") or "")
+  end
 end)
 
 
